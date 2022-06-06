6274d520bf4b124eb690fcf03c68c66cffa4b24f
MayaSelena773 <maya.bono721@gmail.com>
2022-06-06 18:07:05 +0200

    jsandcomment

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
 
+//Dopo aver compilato il form che chiede all'utente il suo nome, il numero di chilometri che vuole percorrere e l'età del passeggero
+//Al click sul pulsante genera dovrà essere calcolato il prezzo del suo biglietto
 
-const generateButton = document.getElementById ('generate-ticket');
+const generateButton = document.getElementById('generate-ticket');
 generateButton.addEventListener('click',
    function() {
 
     const userName = document.getElementById ('user-name').value;
-    const userKm = parseInt (document.getElementById ('user-km')).value;
-    const userAge = parseInt (document.getElementById ('user-age')).value;
+    const userKm = parseInt (document.getElementById ('user-km').value);
+    const userAge = parseInt (document.getElementById ('user-age').value);
     
+    //Prezzo standard del biglietto nkm*0.21
     const price = userKm * 0.21;
     let discount = 0;
 
-    if (userAge === 'minorenne')
-    discount = price * 2 / 100;
-    else if (userAge === 'over 65')
-    discount = price * 4 / 100;
+    //Applicazione sconti in base all'età: minorenni 20%, over 65 40%
+    if (userAge === 'minorenne'){
+        discount = price * 2 / 100;
 
+    }else if (userAge === 'over') {
+        discount = price * 4 / 100;
+    }
+    
+
+    //Prezzo finale (prezzo - sconto) e stampa in pagina approssimata ai primi due decimali dopo la virgola
     const finalPrice = price - discount;
     console.log(finalPrice);
+    
+    document.getElementById('ticket-name').innerHTML = userName;
+    document.getElementById('ticket-price').innerHTML = finalPrice.toFixed(2);
+    //document.getElementById('ticket').classList.add('active');
 
     }
-
 );
